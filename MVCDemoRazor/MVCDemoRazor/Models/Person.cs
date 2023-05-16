6d2d93a24ccef6e8312c98cee7ce4f1134dca0e4
adlarajasri <129931618+adlarajasri@users.noreply.github.com>
2023-05-16 10:16:15 +0530

    Add files via upload

---
+++ MVCDemoRazor/MVCDemoRazor/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿namespace MVCDemoRazor.Models
+{
+    public class Person
+    {
+        public string Nmae { get; set; }
+        public int Age { get; set; }
+        public Person()
+        {
+            
+        }
+        public Person(string nmae, int age)
+        {
+            Nmae = nmae;
+            Age = age;
+        }
+    }
+}
